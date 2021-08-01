655e03c6d4897b906fa581ae31b539f3518b593b
Langlois, Vince Thomas <vtlanglo@iu.edu>
2021-07-31 22:40:38 -0400

    test for new .js file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// import React from "react";
-// import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom"
 
-// const root = document.querySelector("#react-root");
-// const element = <h1>Hello World!</h1>;
+const root = document.querySelector("#test-root");
+const element = <h1>Hello World!</h1>;
 
-// ReactDOM.render(element, root);
+ReactDOM.render(element, root);
