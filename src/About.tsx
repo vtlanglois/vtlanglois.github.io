f6bc26324efaac3d337435b51097806e207e03c6
Langlois, Vince Thomas <vtlanglo@iu.edu>
2022-08-31 12:14:59 -0400

    added routing

---
+++ src/About.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+    return ( 
+    <div>
+    <Link to="/">Home</Link>
+    <h2>About Page</h2> 
+    </div>
+    );
+   }
+export default About;
--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <Link to="/about">About</Link>
+      <h2>Github Pages</h2>
+      <h3>Deploying React to Github</h3>
     </div>
   );
 }
 
 export default App;
+
+
