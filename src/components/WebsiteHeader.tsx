fe058e364b00c337651725d5296ef9fa01d92375
Langlois, Vince Thomas <vtlanglo@iu.edu>
2022-09-01 09:33:20 -0400

    testing making hamburger button

--- src/components/WebsiteHeader.tsx
+++ src/components/WebsiteHeader.tsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { NavbarBrand, NavLink } from "reactstrap";
+import { Collapse, NavbarBrand, NavbarToggler, NavLink } from "reactstrap";
 //import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./WebsiteHeader.css"
 
 const WebsiteHeader = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+  
     return (
         <div>
         <Navbar bg="dark" variant="dark">
         <Container>
           <NavbarBrand href="#/">vincent langlois</NavbarBrand>
-          <Nav className="ml-auto">
+          <NavbarToggler onClick={toggle}/>
+          <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
             <NavLink href="#/about">📖about</NavLink>
             <NavLink href="#/work">📁work</NavLink>
             <NavLink href="#/contact">✉️contact</NavLink>
           </Nav>
+        </Collapse>
         </Container>
       </Navbar>
       </div>
