a8532deba1769cc9a5c3f9e0ca97eaa2a2511399
Langlois, Vince Thomas <vtlanglo@iu.edu>
2022-09-01 17:48:31 -0400

    Update EmojiSelector.tsx

--- src/components/EmojiSelector.tsx
+++ src/components/EmojiSelector.tsx
@@ -1,13 +1,20 @@
+
 export function SelectEmoji(){
     const currentDay = new Date().getDate();
     const currentMonth = new Date().getMonth()+1;
-    
-    if(currentDay == 12 && currentMonth == 25) return "🎄";
-    if(currentDay == 10 && currentMonth == 31) return "🎃";
-    if(currentDay == 1 && currentMonth == 1) return "🎆";
-    if(currentDay == 4 && currentMonth == 1) return "🤡";
-    if(currentDay == 12 && currentMonth == 15) return "🎂";
-    else return "✌️"
+
+    if(currentMonth == 1 && currentDay == 1) return "🎆";
+    if(currentMonth == 2 && currentDay == 14) return "💘";
+    if(currentMonth == 3 && currentDay == 20) return "🌱"
+    if(currentMonth == 4 && currentDay == 1) return "🤡";
+    if(currentMonth == 5 && currentDay == 5) return "🫔";
+    if(currentMonth == 6 && currentDay == 23) return "☀️";
+    if(currentMonth == 9 && currentDay == 22) return "🍂";
+    if(currentMonth == 10 && currentDay == 31) return "🎃";
+    if(currentMonth == 12 && currentDay == 15) return "🎂";
+    if(currentMonth == 12 && currentDay == 21) return "❄️";
+    if(currentMonth == 12 && currentDay == 25) return "🎄";
+    else return "✌️";
     
 }
 
