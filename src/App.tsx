548a3b5fa8c9258fb193813c2419ceb9b5dc4d78
Langlois, Vince Thomas <vtlanglo@iu.edu>
2022-09-01 12:58:50 -0400

    added filler text to explain what this is

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,10 @@ function App() {
       <header className='App-header'>
       <WebsiteHeader/>
       <h2>Under Construction!</h2>
+      <p>Hello! My name is Vincent Langlois. </p>
+      <p>I am a senior studying computer science at Indiana University</p>
+      <p>I wanted to learn more about Typescript, React, and web design, so I decided to make my own website on Github Pages!</p>
+      <p>When this is completed, this will be the go-to website to learn about me!</p>
       </header>
     </div>
   );
