888b8e478aebab265d2b96eec6d2a6651ef3f7ce
Langlois, Vince Thomas <vtlanglo@iu.edu>
2022-09-04 23:55:39 -0400

    prepping for holiday API to work

--- src/components/EmojiSelector.tsx
+++ src/components/EmojiSelector.tsx
@@ -1,12 +1,21 @@
 
 export function SelectEmoji(){
 
-    var currentMonth = new Date().getMonth()+1;
-    var currentDay = new Date().getDate();
-
+    var currentDate = new Date();
+    var currentMonth = currentDate.getMonth()+1;
+    var currentDay = currentDate.getDate();
+    var currentYear = currentDate.getFullYear();
+    const data = getData(currentMonth, currentDay, currentYear);
     /**
      * @TODO rewrite this to use a holiday API call.
      */
+
+     data.then((response) => {
+        console.log(response[0]["name"]);
+        if(response[0]["name"] === "World Sexual Health Day") return "A";
+        else console.log
+    });
+    
     if(currentMonth == 1 && currentDay == 1) return "🎆";
     if(currentMonth == 2 && currentDay == 14) return "💘";
     if(currentMonth == 3 && currentDay == 20) return "🌱"
@@ -19,5 +28,13 @@ export function SelectEmoji(){
     if(currentMonth == 12 && currentDay == 25) return "🎄";
     else return "✌️";
     
+
+    
+}
+
+async function getData(month: number, day: number, year: number) {
+    const response = await fetch('https://holidays.abstractapi.com/v1/?api_key=7e2fea7dd76d440ebb65a75476e11e63&country=US&year='+year+'&month='+month+'&day='+day, {method: 'GET', headers: {Accept: 'application/json'}});
+    const data = await response.json()
+    return data;
 }
 
