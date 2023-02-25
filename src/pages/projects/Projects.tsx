597e8ed789d3a1fd95d20fb3ef82669176adf88b
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-02-25 11:38:51 -0500

    removing projects from public site

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -7,6 +7,7 @@ function Projects() {
       <body className="App-body">
         <Container>
           <h1>Page under construction!</h1>
+          <hr />
           <Row>
             <Col>
             <Card body className="cardgroup text-center">
@@ -14,6 +15,41 @@ function Projects() {
               <img alt="Card" src="https://picsum.photos/300/200" />
               <CardTitle tag="h1">ATLAS M-1</CardTitle>
               <p>AAA</p>
+              </CardBody>
+            </Card>
+            </Col>
+            <Col>
+            <Card body className="cardgroup text-center">
+              <CardBody>
+              <img alt="Card" src="https://picsum.photos/300/200" />
+              <CardTitle tag="h1">ATLAS M-1</CardTitle>
+              <p>AAA</p>
+              </CardBody>
+            </Card>
+            </Col>
+            <Col>
+            <Card body className="cardgroup text-center">
+              <CardBody>
+              <img alt="Card" src="https://picsum.photos/300/200" />
+              <CardTitle tag="h1">ATLAS M-1</CardTitle>
+              <p>AAA</p>
+              </CardBody>
+            </Card>
+            </Col>
+          </Row>
+          <hr />
+          <h1>Game Design & Development</h1>
+          <hr />
+          <Row>
+            <Col>
+            <Card body className="cardgroup text-center">
+              <CardBody>
+              <img alt="Card" src="https://picsum.photos/300/200" />
+              <CardTitle tag="h1">ATLAS M-1</CardTitle>
+              <p>Created as my final project for MSCH-G320, Altas M-1 is a 3-to-5 minute art and sound experience made for MSCH-G320 
+                at Indiana University Bloomington for my Spring 2022 semester. 
+                This experience was made to challenge me to make an unsettling atmosphere using SFX, ambiance, and visual effects.
+              </p>
               </CardBody>
             </Card>
             </Col>
