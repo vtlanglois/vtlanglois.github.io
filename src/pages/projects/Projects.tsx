83666a6fd63c2a8edebc28daba410b73400078de
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-05-25 18:40:33 -0400

    updated projects page

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -18,7 +18,7 @@ import {
   faGear,
 } from "@fortawesome/free-solid-svg-icons";
 import ProjectCard from "../../components/projects/ProjectCard/ProjectCard";
-import { ar_legocontroller, ar_medflow, ar_oledDemo, gd_atlasm1, gd_flockofcats, sw_pybot, sw_website, sw_xinu, ui_cardinal, ui_cordiall, ui_websiteredesign} from "../../components/projects/DetailedProjects";
+import { ar_legocontroller, ar_medflow, ar_oledDemo, gd_atlasm1, gd_flockofcats, sw_gwd, sw_pybot, sw_website, sw_xinu, ui_cardinal, ui_cordiall, ui_websiteredesign} from "../../components/projects/DetailedProjects";
 function Projects() {
   return (
     <div>
@@ -28,10 +28,11 @@ function Projects() {
           <TabHeader title="Software Engineering" icon={faCode} />
           {/* <ProjectCard {...sw_xinu}/><br/> */}
           <ProjectCard {...sw_pybot}/><br/>
-          <ProjectCard {...sw_website}/>
+          <ProjectCard {...sw_website}/><br/>
+          <ProjectCard {...sw_gwd}/>
           <TabHeader title="UI/UX" icon={faBorderAll} />
           <ProjectCard {...ui_cordiall}/><br/>
-          <ProjectCard {...ui_websiteredesign}/><br/>
+          <ProjectCard {...ui_websiteredesign}/>
           {/* <ProjectCard {...ui_cardinal}/> */}
           <TabHeader title="Game Design & Development" icon={faGamepad} />
           <ProjectCard {...gd_flockofcats}/><br/>
