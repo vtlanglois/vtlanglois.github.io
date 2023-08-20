6ddb2217266901207f9218bc1b05ffd6330440cd
Vincent Langlois <vtlanglo@gmail.com>
2023-08-20 13:18:43 -0400

    made Education section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from "./layout/About";
 import Button from "./components/Button";
 import SkillsList from "./layout/Skills";
 import Contact from "./layout/Contact";
+import Education from "./layout/Education";
 
 function App() {
   return (
@@ -15,32 +16,7 @@ function App() {
       </header>
       <section className="main-content">
         <About />
-        <section>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <h2>Education</h2>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 col-sm-6">
-                <h5>Indiana University, Bloomington, Indiana</h5>
-              </div>
-              <div className="col-md-6 col-sm-6 text-sm-end">
-                <h6>Aug '23 - Present</h6>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <i>
-                    <h6>Bachelors of Science in Computer Science</h6>
-                    <h6>&#8627; Specialization in Software Engineering</h6>
-                    <h6>Minor in Game Design</h6>
-                  </i>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+       <Education />
         <section>
           <div className="container">
             <div className="row">
---
+++ src/layout/Education.jsx
@@ -0,0 +1,34 @@
+import SectionHeader from "../components/SectionHeader";
+
+const Education = () => {
+    return (
+        <section>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <SectionHeader text={"Education"}/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-6 col-sm-6">
+              <h5>Indiana University, Bloomington, Indiana</h5>
+            </div>
+            <div className="col-md-6 col-sm-6 text-sm-end">
+              <h6>Aug '23 - Present</h6>
+            </div>
+            <div className="row">
+              <div className="col">
+                <i>
+                  <h6>Bachelors of Science in Computer Science</h6>
+                  <h6>&#8627; Specialization in Software Engineering</h6>
+                  <h6>Minor in Game Design</h6>
+                </i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+export default Education;
