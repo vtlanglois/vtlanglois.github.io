d5c9d98407c29b3b96e1515bba0a75eb33a0e33d
Vincent Langlois <vtlanglo@gmail.com>
2023-08-20 08:46:12 -0400

    aded RWD to buttons and intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Button from "./components/Button";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./sass/App.scss";
-import CV from "./vincent-langlois-cv.pdf"
+import CV from "./vincent-langlois-cv.pdf";
 
 function App() {
   return (
@@ -15,43 +15,39 @@ function App() {
         <div className="container">
           <div className="row">
             <div className="col-md-5 col-sm-12">
-              <h1>Hi! 👋😄</h1>
-              <h1>I'm Vincent Langlois.</h1>
-              <h2>I build things, often using</h2>
-              <h2>
-                <code>{"[arrays,]"}</code> and <code>{"{...objects}"}</code>.
-              </h2>
+              <div className="intro-container">
+                <h1>Hi! 👋😄</h1>
+                <h1>I'm Vincent Langlois.</h1>
+                <h2>I build things, often using</h2>
+                <h2>
+                  <code>{"[arrays,]"}</code> and <code>{"{...objects}"}</code>.
+                </h2>
+              </div>
             </div>
             <div className="col-md-7 col-sm-12">
               <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Dolores error quaerat voluptate, pariatur magni hic mollitia
+                debitis officia at asperiores vero magnam, rem distinctio itaque
+                velit quam sint quo nemo. Lorem ipsum dolor sit, amet
+                consectetur adipisicing elit. Dolores error quaerat voluptate,
+                pariatur magni hic mollitia debitis officia at asperiores vero
+                magnam, rem distinctio itaque velit quam sint quo nemo. Lorem
+                ipsum dolor sit, amet consectetur adipisicing elit. Dolores
                 error quaerat voluptate, pariatur magni hic mollitia debitis
                 officia at asperiores vero magnam, rem distinctio itaque velit
-                quam sint quo nemo. Lorem ipsum dolor sit, amet consectetur
-                adipisicing elit. Dolores error quaerat voluptate, pariatur magni
-                hic mollitia debitis officia at asperiores vero magnam, rem
-                distinctio itaque velit quam sint quo nemo. Lorem ipsum dolor sit,
-                amet consectetur adipisicing elit. Dolores error quaerat
-                voluptate, pariatur magni hic mollitia debitis officia at
-                asperiores vero magnam, rem distinctio itaque velit quam sint quo
-                nemo.
+                quam sint quo nemo.
               </p>
             </div>
             <div className="row">
               <div className="col">
                 <div className="button-container">
-                  <Button
-                    href={CV}
-                    label="My CV"
-                  />
+                  <Button href={CV} label="My CV" />
                   <Button
                     href="https://www.linkedin.com/in/vtlanglois/"
                     label="LinkedIN"
                   />
-                  <Button
-                    href="https://github.com/vtlanglois"
-                    label="GitHub"
-                  />
+                  <Button href="https://github.com/vtlanglois" label="GitHub" />
                 </div>
               </div>
             </div>
