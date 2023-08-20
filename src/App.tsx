eeefccab0a466904e9dac4db41ed11e4264d8a55
Vincent Langlois <vtlanglo@gmail.com>
2023-08-20 00:24:02 -0400

    working on buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,47 @@ import "./sass/App.scss";
 
 function App() {
   return (
-    <div className="container">
+    <div>
       <header>
-        <h1>Back to One</h1>
+        <div className="container">
+          <h1>Back to One</h1>
+        </div>
       </header>
-      <div className="row">
-        <div className="col-md-5 col-sm-12">
-          <h1>Hi! 👋😄</h1>
-          <h1>I'm Vincent Langlois.</h1>
-          <h2>I build things, often using</h2>
-          <h2><code>{"[arrays,]"}</code> and <code>{"{...objects}"}</code>.</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-5 col-sm-12">
+            <h1>Hi! 👋😄</h1>
+            <h1>I'm Vincent Langlois.</h1>
+            <h2>I build things, often using</h2>
+            <h2>
+              <code>{"[arrays,]"}</code> and <code>{"{...objects}"}</code>.
+            </h2>
+          </div>
+          <div className="col-md-7 col-sm-12">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+              error quaerat voluptate, pariatur magni hic mollitia debitis
+              officia at asperiores vero magnam, rem distinctio itaque velit
+              quam sint quo nemo. Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Dolores error quaerat voluptate, pariatur magni
+              hic mollitia debitis officia at asperiores vero magnam, rem
+              distinctio itaque velit quam sint quo nemo. Lorem ipsum dolor sit,
+              amet consectetur adipisicing elit. Dolores error quaerat
+              voluptate, pariatur magni hic mollitia debitis officia at
+              asperiores vero magnam, rem distinctio itaque velit quam sint quo
+              nemo.
+            </p>
+          </div>
+          <div className="row">
+            <div className="col">
+              <button>Click Me</button>
+            </div>
+          </div>
         </div>
-        <div className="col-md-7 col-sm-12">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-            error quaerat voluptate, pariatur magni hic mollitia debitis officia
-            at asperiores vero magnam, rem distinctio itaque velit quam sint quo
-            nemo.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-            error quaerat voluptate, pariatur magni hic mollitia debitis officia
-            at asperiores vero magnam, rem distinctio itaque velit quam sint quo
-            nemo.
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
-            error quaerat voluptate, pariatur magni hic mollitia debitis officia
-            at asperiores vero magnam, rem distinctio itaque velit quam sint quo
-            nemo.
-          </p>
-        </div>
+        <footer>
+          <h1></h1>
+        </footer>
       </div>
-      <footer>
-        <h1>
-          
-        </h1>
-      </footer>
     </div>
   );
 }
