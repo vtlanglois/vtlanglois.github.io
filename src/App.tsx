ebce99be0c83aecec066d0bc5901372c791523d4
Vincent Langlois <vtlanglo@gmail.com>
2023-08-19 22:50:08 -0400

    added SASS to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import { Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div>
+    <div className="container App-body">
       <h1>Back to One</h1>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit
+          ex assumenda iure sint animi perferendis iusto natus molestias minus
+          nesciunt facere, eos, tenetur optio voluptatibus saepe quod
+          repellendus fugit?
+        </p>
+      </div>
+
+      <div className="row">
+        <div className="col">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+            error quaerat voluptate, pariatur magni hic mollitia debitis officia
+            at asperiores vero magnam, rem distinctio itaque velit quam sint quo
+            nemo.
+          </p>
+        </div>
+        <div className="col">
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+            error quaerat voluptate, pariatur magni hic mollitia debitis officia
+            at asperiores vero magnam, rem distinctio itaque velit quam sint quo
+            nemo.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
-import "./App.css";
+import "./App.scss";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
