a79f9d2cc617dfc79062821c2fb61e2f0ed856e3
Vincent Langlois <vtlanglo@gmail.com>
2023-08-19 23:44:03 -0400

    removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./sass/App.scss";
 
@@ -8,25 +7,23 @@ function App() {
       <header>
         <h1>Back to One</h1>
       </header>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit
-          ex assumenda iure sint animi perferendis iusto natus molestias minus
-          nesciunt facere, eos, tenetur optio voluptatibus saepe quod
-          repellendus fugit?
-        </p>
-      </div>
       <div className="row">
         <div className="col-md-5 col-sm-12">
+          <h1>Hi! 👋😄</h1>
+          <h1>I'm Vincent Langlois.</h1>
+          <h2>I build things, often using</h2>
+          <h2><code>{"[arrays,]"}</code> and <code>{"{...objects}"}</code>.</h2>
+        </div>
+        <div className="col-md-7 col-sm-12">
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
             error quaerat voluptate, pariatur magni hic mollitia debitis officia
             at asperiores vero magnam, rem distinctio itaque velit quam sint quo
             nemo.
-          </p>
-        </div>
-        <div className="col-md-7 col-sm-12">
-          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+            error quaerat voluptate, pariatur magni hic mollitia debitis officia
+            at asperiores vero magnam, rem distinctio itaque velit quam sint quo
+            nemo.
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
             error quaerat voluptate, pariatur magni hic mollitia debitis officia
             at asperiores vero magnam, rem distinctio itaque velit quam sint quo
@@ -36,7 +33,7 @@ function App() {
       </div>
       <footer>
         <h1>
-          End of One
+          
         </h1>
       </footer>
     </div>
