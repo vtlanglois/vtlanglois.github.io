964cd28c5c222c0c9d4ee963876a5ba42b78afbd
Vincent Langlois <vtlanglo@gmail.com>
2023-11-24 12:31:20 -0500

    Added spacing below section headers

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -1,7 +1,7 @@
 const SectionHeader = ({text, className}) => {
     return (
         <div>
-            <h2 className={`section-header ${className}`}>{text}</h2>
+            <h2 className={`section-header mb-lg-3 ${className}`}>{text}</h2>
         </div>
     );
 }
