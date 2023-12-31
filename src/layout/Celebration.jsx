a71873aacf1a7094fdb8399271cf31d380fee0f8
Vincent Langlois <vtlanglo@gmail.com>
2023-12-30 22:51:53 -0500

    Fixed keys not being strings

--- src/layout/Celebration.jsx
+++ src/layout/Celebration.jsx
@@ -2,31 +2,31 @@ import React from "react";
 import Emoji from "../components/Emoji";
 
 const holidays = {
-  "11": { 
+  "11": {
     startEmoji: "🎆",
     text: "Happy New Years!",
-    endEmoji: "🎇"
+    endEmoji: "🎇",
   },
   "214": {
     startEmoji: "💘",
     text: "Happy Valentines Day!",
-    endEmoji: "💝"
+    endEmoji: "💝",
   },
   "317": {
     startEmoji: "🍀",
     text: "Happy St. Patrick's Day!",
-    endEmoji: "🍀"
+    endEmoji: "🍀",
   },
   "41": {
     startEmoji: "🤡",
     text: "April Fools!!!",
-    endEmoji: "🤡"
+    endEmoji: "🤡",
   },
   "422": {
     startEmoji: "🌎",
     text: "Happy Earth Day!",
-    endEmoji: "🌍"
-  }
+    endEmoji: "🌍",
+  },
 };
 
 const Celebration = () => {
@@ -34,37 +34,39 @@ const Celebration = () => {
   var currentMonth = currentDate.getMonth() + 1;
   var currentDay = currentDate.getDate();
   var holidayKey = currentMonth.toString() + currentDay.toString();
-  var holiday = holidays[holidayKey]
+  var holiday = holidays[holidayKey];
   if (holiday) {
-
-    return (<section>
-      <div className="container my-lg-4">
-        <div className="row text-center">
-          <div className="col-12">
-            <CelebrationText startEmoji={holiday.startEmoji} endEmoji={holiday.endEmoji}>
-              {holiday.text}
-            </CelebrationText>
+    return (
+      <section>
+        <div className="container my-lg-4">
+          <div className="row text-center">
+            <div className="col-12">
+              <CelebrationText
+                startEmoji={holiday.startEmoji}
+                endEmoji={holiday.endEmoji}
+              >
+                {holiday.text}
+              </CelebrationText>
+            </div>
           </div>
         </div>
-      </div>
-    </section>);
+      </section>
+    );
   }
 
   return <></>;
 };
 
-const CelebrationText = ({startEmoji, endEmoji, children}) => {
+const CelebrationText = ({ startEmoji, endEmoji, children }) => {
   return (
     <p className="h1">
       <Emoji>{startEmoji}</Emoji>
       <b>
-        <u>
-          {children}
-        </u>
+        <u>{children}</u>
       </b>
       <Emoji className="ms-1">{endEmoji}</Emoji>
     </p>
-  )
-}
+  );
+};
 
 export default Celebration;
