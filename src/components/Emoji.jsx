54602d9fa08f6af9aa059bf064d99070341a7207
Vincent Langlois <vtlanglo@gmail.com>
2024-01-01 14:03:50 -0500

    fixed classname undefined in Emoji

--- src/components/Emoji.jsx
+++ src/components/Emoji.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const Emoji = ({ className, children }) => {
   return (
-    <span className={`me-1 ${className}`} role="img" alt="" aria-hidden="true">
+    <span
+      className={className ? `me-1 ${className}` : `me-1`}
+      role="img"
+      alt=""
+      aria-hidden="true"
+    >
       {children}
     </span>
   );
