4fbfd9ef8c1c6e011bea118614e5d4e037720db0
Vincent Langlois <vtlanglo@gmail.com>
2024-03-10 14:54:27 -0400

    Added props

--- src/layout/SemesterTable.jsx
+++ src/layout/SemesterTable.jsx
@@ -1,31 +1,31 @@
-import React from 'react';
+import React from "react";
 
-const SemesterTable = () => {
+const SemesterTable = ({ semester, headerText }) => {
   return (
-      <div className="col-lg-6 col-12">
-        <table className='table table-sm table-striped'>
-          <thead>
-            <tr>
-              <th>Class</th>
-              <th>Number</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Intro to CS</td>
-              <td>CSCI-H211</td>
-              <td>A</td>
-            </tr>
-            <tr>
-              <td>Intro to CS</td>
-              <td>CSCI-H211</td>
-              <td>A</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div className="col-lg-6 col-12">
+      <h3 >{headerText}</h3>
+      <table className="table table-sm table-striped">
+        <thead>
+          <tr>
+            <th>Class</th>
+            <th>Number</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {semester.map((item) => {
+            return (
+              <tr>
+                <td>AAA</td>
+                <td>AAA</td>
+                <td>AAA</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
-}
+};
 
 export default SemesterTable;
--- src/layout/TranscriptYear.jsx
+++ src/layout/TranscriptYear.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import SectionHeader from '../components/SectionHeader';
 import SemesterTable from './SemesterTable';
+import Emoji from '../components/Emoji';
 
 const TranscriptYear = ({headerText}) => {
   return (
     <section className='container'>
       <SectionHeader>{headerText} </SectionHeader>
       <div className="row">
-        <SemesterTable />
-        <SemesterTable />
+        <SemesterTable semester={[1,2,3]} headerText={<><Emoji>🍁</Emoji> Fall Semester</>}/>
+        <SemesterTable semester={[1,2,3]} headerText={<><Emoji>🌸</Emoji> Spring Semester</>}/>
       </div>
     </section>
   );
