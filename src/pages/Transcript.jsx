e34e2e71e9a6a65625c7c240152faf08b64295e2
Vincent Langlois <vtlanglo@gmail.com>
2024-03-10 15:01:51 -0400

    Added RD for full page

--- src/pages/Transcript.jsx
+++ src/pages/Transcript.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import TranscriptYear from '../layout/TranscriptYear';
-import Emoji from '../components/Emoji';
+import React from "react";
+import TranscriptYear from "../layout/TranscriptYear";
+import Emoji from "../components/Emoji";
 
 const Transcript = () => {
   return (
     <>
-    <div className="container">
-      <div className="row">
-        <h1 className="col-12">Transcript</h1>
+      <div className="container">
+        <div className="row">
+          <h1 className="col-12">Transcript</h1>
+        </div>
       </div>
-    </div>
-      <TranscriptYear headerText={
-        <><Emoji>🥚</Emoji> Freshman Year</>
-      }/>
+      <TranscriptYear
+        headerText={
+          <>
+            <Emoji>🥚</Emoji> Freshman Year
+          </>
+        }
+      />
+      <TranscriptYear
+        headerText={
+          <>
+            <Emoji>🐣</Emoji> Sophomore Year
+          </>
+        }
+      />
+      <TranscriptYear
+        headerText={
+          <>
+            <Emoji>🐥</Emoji> Junior Year
+          </>
+        }
+      />
+      <TranscriptYear
+        headerText={
+          <>
+            <Emoji>🕊️</Emoji> Senior Year
+          </>
+        }
+      />
     </>
   );
-}
+};
 
 export default Transcript;
